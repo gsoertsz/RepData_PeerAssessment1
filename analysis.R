a526a16c3bc490b557b8bb654a7f0a0f0bddfabe
Greg D Soertsz <gsoertsz@Gregs-MacBook-Pro.local>
2016-02-08 09:15:44 +1100

    mean, median, histogram of daily total

--- analysis.R
+++ analysis.R
@@ -5,5 +5,21 @@ ensureActivityData <- function(srcFileName = "activity.csv", archiveName = "acti
     unzip(zipfile = file.path(baseDir, archiveName), overwrite = TRUE, exdir = baseDir);
   }
   
-  return (read.csv(activitySrcPath))
+  return (read.csv(activitySrcPath, stringsAsFactors = TRUE))
+}
+
+main <- function() {
+  df <- ensureActivityData();
+  
+  cleanDf <- df[(which(!is.na(df$steps))), ]
+  nonZeroClean <- df[(cleanDf$steps != 0), ]
+  # histogram of daily steps taken
+  
+  dailySum <- aggregate(nonZeroClean$steps, by=list(date = nonZeroClean$date), FUN = sum);
+  dailyMedian <- aggregate(nonZeroClean$steps, by=list(date = nonZeroClean$date), FUN = median);
+  dailyMean <- aggregate(nonZeroClean$steps, by=list(date = nonZeroClean$date), FUN = mean);
+  
+  # TODO Format this!
+  dailySumHist <- ggplot(dailySum) + geom_histogram(aes(x))
+  print(dailySumHist)
 }
