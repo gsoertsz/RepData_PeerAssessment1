54aa9354dfb9ce8166df342ba184e7d368ec0456
Greg D Soertsz <gsoertsz@Gregs-MacBook-Pro.local>
2016-02-09 15:04:18 +1100

    timeseries

--- analysis.R
+++ analysis.R
@@ -39,12 +39,54 @@ main <- function() {
   # all NA's get the daily average
   adjustedActivity <- data.frame(df, sapply(df$date, addAverageForRespectiveDay, averages=dailyMean));
   names(adjustedActivity) <- c("steps_unadjusted", "date", "interval", "steps");
-
+  
+  # Make a histogram of the total number of steps taken each day and Calculate and report the mean and 
+  # median total number of steps taken per day. Do these values differ from the estimates from the first 
+  # part of the assignment? What is the impact of imputing missing data on the estimates of the total
+  # daily number of steps?
+  adjustedDailyTotal <- aggregate(adjustedActivity$steps, by=list(date = adjustedActivity$date), FUN = "sum")
+  names(adjustedDailyTotal) <- c("Date", "Total");
+  adjustedDailyTotalHist <- ggplot(adjustedDailyTotal) + geom_histogram(aes(Total), binwidth=500);
+  print(adjustedDailyTotalHist);
+  
+  adjustedDailyMedian <- aggregate(adjustedActivity$steps, by=list(date = adjustedActivity$date), FUN = median);
+  names(adjustedDailyMedian) <- c("Date", "Median")
+  adjustedDailyMean <- aggregate(adjustedActivity$steps, by=list(date = adjustedActivity$date), FUN = mean);
+  names(adjustedDailyMean) <- c("Date", "Mean");
+  
+  # Create a new factor variable in the dataset with two levels -- "weekday" and "weekend" 
+  # indicating whether a given date is a weekday or weekend day.
+  dayType <- factor(c("Weekend", "Weekday"));
+  calendarizedAdjustedActivity <- data.frame(adjustedActivity, type = sapply(adjustedActivity$date, dayTypeFunc));
+  
+  padded <- data.frame(calendarizedAdjustedActivity, padded_interval = sapply(calendarizedAdjustedActivity$interval, zeroPadInterval));
+  justDateTime <- data.frame(date = padded$date, time = padded$padded_interval);
+  dateStrings <- paste(justDateTime$date, justDateTime$time);
+  parsed_date <- strptime(dateStrings, "%F %H%M");
+  
+  timeSeriesActivity <- data.frame(calendarizedAdjustedActivity, actual_date = parsed_date);
+  
   # Make a time series plot (i.e. typed = "l") of the 5-minute interval (x-axis) and the 
   # average number of steps taken, average across all days (y-axis)
+  p <- ggplot(timeSeriesActivity) + geom_line(aes(steps, actual_date))
+  print(p)  
+  
   # TODO
-  
-  
+}
+
+zeroPadInterval <- function(intervalStr) {
+  zeros <- rep(0, 4-nchar(as.character(intervalStr)));
+  result <- paste0(paste0(zeros, collapse=""), as.character(intervalStr), collapse="")
+  return (result);
+}
+
+# takes a date string and returns a string (level) indicating whether its a weekday or a weekend
+dayTypeFunc <- function(date) {
+  d <- strptime(date, format = "%F");
+  day <- weekdays(d);
+  if (day == "Saturday" | day == "Sunday") {
+    return ("Weekend")
+  } else return ("Weekday");
 }
 
 addAverageForRespectiveDay <- function(date, averages) {
